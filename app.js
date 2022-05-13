3adaf1cd368f07d5643c77dadc2f9bb11af8cb5b
Anglenis-hub <1346938001@qq.com>
2022-05-13 22:46:04 +0800

    小bug

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ App({
     wx.cloud.init({
       env: "cloud1-2ghfekqc5cf347a4"
     })
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
     // 保存用户openid
     wx.cloud.callFunction({
